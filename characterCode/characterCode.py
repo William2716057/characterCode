96a34f71ac0b002e053c1421d020b400dff321b2
User <Willspri@gmail.com>
2024-04-12 14:27:04 +1200

    Add project files.

---
+++ characterCode/characterCode.py
@@ -0,0 +1,24 @@
+def stringToIntArray(inputString):
+    int_array = []
+
+    for char in inputString:
+        charCode = ord(char)
+        int_array.append(charCode)
+
+    return int_array
+
+def intToHex(int_array):
+    hex_array = []
+
+    for num in int_array:
+        hex_representation = hex(num)[2:]  # Convert integer to hexadecimal and remove '0x' prefix
+        hex_array.append(hex_representation)
+
+    return hex_array
+
+input_string = "abcdeABCDE"
+result = stringToIntArray(input_string)
+print(result)
+
+hexResult = intToHex(result)
+print(hexResult)
