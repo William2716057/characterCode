93a12e4dbb7adbea5cc0fbf8ba8da2ea83b9e6f4
User <Willspri@gmail.com>
2024-04-12 14:51:50 +1200

    base64 added

--- characterCode/characterCode.py
+++ characterCode/characterCode.py
@@ -1,3 +1,5 @@
+import base64
+
 def stringToIntArray(inputString): #converts to decimal
     int_array = []
 
@@ -16,6 +18,11 @@ def intToHex(int_array):
 
     return hex_array
 
+def int_array_to_base64(int_array):
+    bytes_array = bytes(int_array)
+    base64_string = base64.b64encode(bytes_array).decode('utf-8')
+    return base64_string
+
 
 input_string = "abcdeABCDE" #change string here
 result = stringToIntArray(input_string)
@@ -23,5 +30,9 @@ print(result)
 
 hexResult = intToHex(result)
 print(hexResult)
+
+base64_result = int_array_to_base64(result)
+print(base64_result)
+
 
 
